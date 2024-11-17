0c4ae12de1d72733d03c3e0831d4897a58b44382
prudvikeshav <prudhvi_keshav@outlook.com>
2024-11-17 14:26:40 +0530

    modified yaml file

--- frontend/vistorcount.js
+++ frontend/vistorcount.js
@@ -3,7 +3,7 @@ async function get_visitors() {
     // call post api request function
     //await post_visitor();
     try {
-        let response = await fetch('https://prp5y3r6bj.execute-api.us-east-1.amazonaws.com/default/VisitorCounter', {
+        let response = await fetch(' https://dje3905vek.execute-api.us-east-1.amazonaws.com/default/VisitorCounter', {
             method: 'GET',
         });
         let data = await response.json()
